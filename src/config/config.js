d4098f47e7bb282ab7e1b38647f756b42c552a61
Melqui Brito <Merkulino11@gmail.com>
2023-04-20 14:54:49 -0300

    config - tentando fazer deploy no railway sem docker

--- src/config/config.js
+++ src/config/config.js
@@ -1,18 +1,9 @@
-// const environment = process.env.NODE_ENV || 'test';
-
-// const suffix = {
-//   dev: '-dev',
-//   development: '-dev',
-//   test: '-test',
-// };
-
-const options = {
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'blogs-api'}`, // ${suffix[environment] || suffix.test}`,
+const config = {
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
+  database: process.env.MYSQL_DB_NAME || 'blogs-api',
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQL_PORT || 3306,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
@@ -21,10 +12,7 @@ const options = {
 };
 
 module.exports = {
-  development: {
-    ...options,
-  },
-  test: {
-    ...options,
-  },
+  development: config,
+  test: config,
+  production: config,
 };
